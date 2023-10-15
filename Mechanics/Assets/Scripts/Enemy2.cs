0d6b95a6194412e7868891aa7dc6216ad0a357c9
Sinideass <146747710+Sinideass@users.noreply.github.com>
2023-10-15 15:44:40 -0600

    jarronxd

--- Mechanics/Assets/Scripts/Enemy2.cs
+++ Mechanics/Assets/Scripts/Enemy2.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyState
+{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+
 public class Enemy2 : MonoBehaviour
 {
-    public int health;
+
+    public EnemyState currentState;
+    public FloatValue maxHealth;
+    public float health;
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
