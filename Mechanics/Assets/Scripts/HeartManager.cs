12ad73bfdff819165d0117e62a89bdae29c1af76
Antonio Mayorga <128397815+AntonioMV253@users.noreply.github.com>
2023-10-31 12:24:32 -0600

    vida aumentar

--- Mechanics/Assets/Scripts/HeartManager.cs
+++ Mechanics/Assets/Scripts/HeartManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HeartManager : MonoBehaviour {
+public class HeartManager : MonoBehaviour
+{
 
     public Image[] hearts;
     public Sprite fullHeart;
@@ -12,37 +13,42 @@ public class HeartManager : MonoBehaviour {
     public FloatValue heartContainers;
     public FloatValue playerCurrentHealth;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         InitHearts();
-	}
+    }
 
     public void InitHearts()
     {
-        int maxHearts = Mathf.Min(hearts.Length, Mathf.CeilToInt(heartContainers.initialValue));
-
-        for (int i = 0; i < maxHearts; i++)
+        for (int i = 0; i < heartContainers.RuntimeValue; i++)
         {
-            hearts[i].gameObject.SetActive(true);
-            hearts[i].sprite = fullHeart;
+            if (i < hearts.Length)
+            {
+                hearts[i].gameObject.SetActive(true);
+                hearts[i].sprite = fullHeart;
+            }
         }
     }
 
-
     public void UpdateHearts()
     {
+        InitHearts();
         float tempHealth = playerCurrentHealth.RuntimeValue / 2;
-        for (int i = 0; i < heartContainers.initialValue; i ++)
+        for (int i = 0; i < heartContainers.RuntimeValue; i++)
         {
-            if(i <= tempHealth-1)
+            if (i <= tempHealth - 1)
             {
                 //Full Heart
                 hearts[i].sprite = fullHeart;
-            }else if( i >= tempHealth)
+            }
+            else if (i >= tempHealth)
             {
                 //empty heart
                 hearts[i].sprite = emptyHeart;
-            }else{
+            }
+            else
+            {
                 //half full heart
                 hearts[i].sprite = halfFullHeart;
             }
