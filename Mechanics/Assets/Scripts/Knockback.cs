7d742ea0b3463f79ae14ab43751227b774e8d797
Antonio Mayorga <128397815+AntonioMV253@users.noreply.github.com>
2023-11-05 18:02:51 -0600

    knockback mejorado

--- Mechanics/Assets/Scripts/Knockback.cs
+++ Mechanics/Assets/Scripts/Knockback.cs
@@ -1,38 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Knockback : MonoBehaviour
 {
-    public float thrust;
-    public float knockTime;
+
+    [SerializeField] private float thrust;
+    [SerializeField] private float knockTime;
+    [SerializeField] private string otherTag;
     public float damage;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("enemy") || other.gameObject.CompareTag("Player"))
+        /*
+        if (other.gameObject.CompareTag("breakable") && this.gameObject.CompareTag("Player"))
         {
-            Rigidbody2D hit = other.GetComponent<Rigidbody2D>();
+            other.GetComponent<pot>().Smash();
+        }
+        */
+        if (other.gameObject.CompareTag(otherTag) && other.isTrigger)
+        {
+            Rigidbody2D hit = other.GetComponentInParent<Rigidbody2D>();
             if (hit != null)
             {
-                Vector2 difference = hit.transform.position - transform.position;
+                Vector3 difference = hit.transform.position - transform.position;
                 difference = difference.normalized * thrust;
-                hit.AddForce(difference, ForceMode2D.Impulse);
+                hit.DOMove(hit.transform.position + difference, knockTime);
+                //hit.AddForce(difference, ForceMode2D.Impulse);
 
                 if (other.gameObject.CompareTag("enemy") && other.isTrigger)
                 {
                     hit.GetComponent<Enemy2>().currentState = EnemyState.stagger;
                     other.GetComponent<Enemy2>().Knock(hit, knockTime, damage);
                 }
-                if (other.gameObject.CompareTag("Player"))
+
+
+                if (other.GetComponentInParent<PlayerMovement>().currentState != PlayerState.Stagger)
                 {
-                    if (other.GetComponent<PlayerMovement>().currentState != PlayerState.Stagger)
-                    {
-                        hit.GetComponent<PlayerMovement>().currentState = PlayerState.Stagger;
-                        other.GetComponent<PlayerMovement>().Knock(knockTime, damage);
-                    }
+                    hit.GetComponent<PlayerMovement>().currentState = PlayerState.Stagger;
+                    other.GetComponent<PlayerMovement>().Knock(knockTime, damage);
                 }
+
             }
         }
     }
+
+
 }
